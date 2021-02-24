a33206f1c5956a989c337142a86c5e65eb83632d
Dwaipayan Roy Chowdhury <75967964+Dwaipayan-R-C@users.noreply.github.com>
2021-02-24 09:21:33 +0530

    Add files via upload

---
+++ inventor-visual studio-cad-automation/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ofd = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(322, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 33)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Click Here"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Face Mating", "Face Flush", "Plane Mating", "Flush Mating"})
+        Me.ComboBox1.Location = New System.Drawing.Point(322, 78)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(108, 24)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(210, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Joining Options"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(313, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Connect To Inventor and create an assembly file"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(26, 266)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 26)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Add Part"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ofd
+        '
+        Me.ofd.FileName = "OpenFileDialog1"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(150, 266)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(98, 26)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Mate"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(230, 127)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Offset Value"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(322, 124)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(107, 22)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(217, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(99, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Select Plane 1"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"XY Plane", "YZ Plane", "XZ Plane"})
+        Me.ComboBox2.Location = New System.Drawing.Point(321, 175)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(108, 24)
+        Me.ComboBox2.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(217, 224)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Select Plane 2"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"XY Plane", "YZ Plane", "XZ Plane"})
+        Me.ComboBox3.Location = New System.Drawing.Point(321, 221)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(108, 24)
+        Me.ComboBox3.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(966, 472)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Eaglestone Mating Demo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ofd As OpenFileDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+End Class
---
+++ inventor-visual studio-cad-automation/Form1.vb
@@ -0,0 +1,156 @@
+﻿Imports Inventor
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Linq
+Imports System.Xml
+Imports System.Reflection
+Imports System.ComponentModel
+Imports System.Collections
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Forms
+Imports System.Drawing
+Imports System.IO
+Imports Microsoft.Win32
+
+
+Public Class Form1
+    Dim inventorApp As Inventor.Application
+    Dim assemblyDoc As AssemblyDocument = Nothing
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            inventorApp = Marshal.GetActiveObject("Inventor.Application")
+        Catch ex As Exception
+            inventorApp = CreateObject("Inventor.Application")
+        End Try
+        MessageBox.Show("Connected with Inventor")
+        assemblyDoc = TryCast(inventorApp.Documents.Add(DocumentTypeEnum.kAssemblyDocumentObject, "", True), PartDocument)
+        inventorApp.Visible = True
+    End Sub
+
+    Dim Directory As String
+    Dim filename As String
+    Public Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If (ofd.ShowDialog() = DialogResult.OK) Then
+                Directory = ofd.FileName
+                Dim FolderPath As String = Directory
+                Debug.WriteLine(Directory)
+            End If
+            filename = Directory
+            Dim cmdMgr As CommandManager
+            cmdMgr = inventorApp.CommandManager
+            Call cmdMgr.PostPrivateEvent(6657, filename)
+            cmdMgr.ControlDefinitions.Item("AssemblyPlaceComponentCmd").Execute()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Function faceMate(ByRef inventorApp As Inventor.Application)
+        Try
+            Dim count As Integer
+            Dim oDoc As AssemblyDocument
+            oDoc = inventorApp.ActiveDocument
+            Dim oFace1 As Face
+            oFace1 = inventorApp.CommandManager.Pick(SelectionFilterEnum.kPartFacePlanarFilter, "Select a cylindrical face")
+            Dim oFace2 As Face
+            oFace2 = inventorApp.CommandManager.Pick(SelectionFilterEnum.kPartFacePlanarFilter, "Select another cylindrical face")
+            Dim omate As MateConstraint
+            omate = oDoc.ComponentDefinition.Constraints.AddMateConstraint(oFace1, oFace2, CInt(Int(TextBox1.Text)))
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Function
+
+    Function faceFlush(ByRef inventorApp As Inventor.Application)
+        Try
+            Dim oDoc As AssemblyDocument
+            oDoc = inventorApp.ActiveDocument
+            Dim oFace1 As Face
+            oFace1 = inventorApp.CommandManager.Pick(SelectionFilterEnum.kPartFacePlanarFilter, "Select a cylindrical face")
+            Dim oFace2 As Face
+            oFace2 = inventorApp.CommandManager.Pick(SelectionFilterEnum.kPartFacePlanarFilter, "Select another cylindrical face")
+            Dim omate As FlushConstraint
+            omate = oDoc.ComponentDefinition.Constraints.AddFlushConstraint(oFace1, oFace2, CInt(Int(TextBox1.Text)))
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            inventorApp = Marshal.GetActiveObject("Inventor.Application")
+        Catch ex As Exception
+            MessageBox.Show("Inventor not connected")
+        End Try
+        Try
+            If Me.ComboBox1.SelectedIndex = 0 Then
+                faceMate(inventorApp)
+            End If
+            If Me.ComboBox1.SelectedIndex = 1 Then
+                faceFlush(inventorApp)
+            End If
+            If Me.ComboBox1.SelectedIndex = 2 Then
+                If (Me.ComboBox2.Text = Nothing) Then
+                    MessageBox.Show("Please Select Plane 1")
+                Else
+                    planeMate(inventorApp)
+                End If
+            End If
+            If Me.ComboBox1.SelectedIndex = 3 Then
+                If (Me.ComboBox3.Text = Nothing) Then
+                    MessageBox.Show("Please Select Plane 2")
+                Else
+                    planeFlush(inventorApp)
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Function planeMate(ByRef inventorApp As Inventor.Application)
+        Try
+            Dim oAsmCompDef As AssemblyComponentDefinition
+            oAsmCompDef = inventorApp.ActiveDocument.ComponentDefinition
+            Dim compOcc1 As ComponentOccurrence = oAsmCompDef.Occurrences.Item(1)
+            Dim compOcc2 As ComponentOccurrence = oAsmCompDef.Occurrences.Item(2)
+            Dim oWorkPlane1 As WorkPlane
+            oWorkPlane1 = compOcc1.Definition.WorkPlanes(ComboBox2.Text)
+            Dim oWorkPlane2 As WorkPlane
+            oWorkPlane2 = compOcc2.Definition.WorkPlanes(ComboBox3.Text)
+            Dim oproxyWorkPlane1 As WorkPlaneProxy = Nothing
+            compOcc1.CreateGeometryProxy(oWorkPlane1, oproxyWorkPlane1)
+            Dim oproxyWorkPlane2 As WorkPlaneProxy = Nothing
+            compOcc2.CreateGeometryProxy(oWorkPlane2, oproxyWorkPlane2)
+            Dim oAConstraint As MateConstraint
+            oAConstraint = oAsmCompDef.Constraints.AddMateConstraint(oproxyWorkPlane1, oproxyWorkPlane2, CInt(Int(TextBox1.Text)))
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Function
+
+    Function planeFlush(ByRef inventorApp As Inventor.Application)
+        Try
+            Dim oAsmCompDef As AssemblyComponentDefinition
+            oAsmCompDef = inventorApp.ActiveDocument.ComponentDefinition
+            Dim compOcc1 As ComponentOccurrence = oAsmCompDef.Occurrences.Item(1)
+            Dim compOcc2 As ComponentOccurrence = oAsmCompDef.Occurrences.Item(2)
+            Dim oWorkPlane1 As WorkPlane
+            oWorkPlane1 = compOcc1.Definition.WorkPlanes(ComboBox2.Text)
+            Dim oWorkPlane2 As WorkPlane
+            oWorkPlane2 = compOcc2.Definition.WorkPlanes(ComboBox3.Text)
+            Dim oproxyWorkPlane1 As WorkPlaneProxy = Nothing
+            compOcc1.CreateGeometryProxy(oWorkPlane1, oproxyWorkPlane1)
+            Dim oproxyWorkPlane2 As WorkPlaneProxy = Nothing
+            compOcc2.CreateGeometryProxy(oWorkPlane2, oproxyWorkPlane2)
+            Dim oAConstraint As FlushConstraint
+            oAConstraint = oAsmCompDef.Constraints.AddFlushConstraint(oproxyWorkPlane1, oproxyWorkPlane2, CInt(Int(TextBox1.Text)))
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Function
+
+End Class
